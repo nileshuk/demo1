357079be979e2e77d2a399943989bf6b1356cb41
Nileshuk <nileshukey5154@gmail.com>
2021-12-21 11:05:11 +0530

    nil

--- GitHub/src/B.java
+++ GitHub/src/B.java
@@ -5,8 +5,8 @@ public class B
 	public static void main(String[] args) 
 	{
 		System.out.println("wlcome to cjc");
-		System.out.println("wlcome to pune");
 		
+	
 	}
 
 }
---
+++ GitHub/src/C.java
@@ -0,0 +1,18 @@
+
+public class C
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("hellow cjc");
+		
+	}
+	
+	public void m1()
+	{
+		System.out.println("testing batch");
+		
+	}
+	
+
+}
